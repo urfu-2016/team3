8ea574df563146148cfc274d774e4d6cc5bf6c92
Gallyam Biktashev <gbiktashev@naumen.ru>
2017-03-22 14:45:00 +0500

    Closes #22. Add registration page

--- routes.js
+++ routes.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const main = require('./controllers/main');
-
+const registration = require('./controllers/registration');
 module.exports = app => {
     app.get('/', main);
+    app.get('/register', registration);
     app.all('*', (req, res) => res.sendStatus(404));
 };
 
