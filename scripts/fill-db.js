3c2540db0192cae0518371d440566b79281e7674
Gallyam Biktashev <gbiktashev@naumen.ru>
2017-04-13 14:38:01 +0500

    fill-db.js refactoring

--- scripts/fill-db.js
+++ scripts/fill-db.js
@@ -4,51 +4,49 @@ const Quest = require('../models/quest');
 const User = require('../models/user');
 const mongoose = require('mongoose');
 
-const removedHandler = err => {
-    if (err) {
-        console.error('Collection was not removed', err);
-    } else {
-        console.info('Removed');
-    }
-};
-
-require('../db/connect')().then(() => {
-    const removePromises = [
-        User.remove({}, removedHandler).exec(),
-        Quest.remove({}, removedHandler).exec()
-    ];
-
-    Promise.all(removePromises)
-        .then(() => {
-            const user = new User({
-                likedQuests: [],
-                name: 'John Doe',
-                email: 'example@mail.com',
-                passwordHash: '3432423sdza',
-                passedQuests: [],
-                photoStatuses: []
-            });
-
-            user.save()
-                .then(() => {
-                    const saves = [];
-                    for (let i = 0; i < 20; i++) {
-                        saves.push(new Quest({
-                            author: user,
-                            creationDate: new Date(1490776 + Math.floor(300000 * Math.random())),
-                            name: `Quest ${i}`,
-                            description: 'Description here, must be more than 30 characters',
-                            likesCount: Math.floor(50 * Math.random()),
-                            passesCount: 2,
-                            photos: []
-                        }).save());
-                    }
-
-                    Promise.all(saves)
-                        .then(() => {
-                            console.log('Done');
-                            mongoose.connection.close();
-                        });
-                });
+require('../db/connect')()
+    .then(() => {
+        return Promise.all([
+            User.remove({}).exec(),
+            Quest.remove({}).exec()
+        ]);
+    })
+    .then(() => {
+        console.info('Collections successfully cleared');
+        const user = new User({
+            likedQuests: [],
+            name: 'John Doe',
+            email: 'example@mail.com',
+            passwordHash: '3432423sdza',
+            passedQuests: [],
+            photoStatuses: []
         });
-});
+
+        return user.save();
+    })
+    .then(user => {
+        console.info('User created');
+        const saves = [];
+        for (let i = 0; i < 20; i++) {
+            saves.push(new Quest({
+                author: user,
+                creationDate: new Date(1490776 + Math.floor(300000 * Math.random())),
+                name: `Quest ${i}`,
+                description: 'Description here, must be more than 30 characters',
+                likesCount: Math.floor(50 * Math.random()),
+                passesCount: 2,
+                photos: [],
+                published: true
+            }).save());
+        }
+
+        return Promise.all(saves);
+    })
+    .then(() => {
+        console.log('Quests created');
+        mongoose.connection.close();
+    })
+    .catch(err => {
+        console.error(err.message, err);
+        mongoose.connection.close();
+    });
