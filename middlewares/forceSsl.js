52295ca187c50b9cee57416017218a3db5d55e0d
Gallyam Biktashev <gallyamb@gmail.com>
2017-04-16 22:10:24 +0500

    Try to fix forceSsl

--- middlewares/forceSsl.js
+++ middlewares/forceSsl.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = (req, res, next) => {
+    console.info(req.header('x-forwarded-proto'));
     if (req.header('x-forwarded-proto') !== 'https') {
         res.redirect(`https://${req.header('host')}${req.url()}`)
     } else {
