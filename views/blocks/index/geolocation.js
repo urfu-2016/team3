ffd681bb800ab20c9cf2699cb94db738a5eaff01
Evgeny Petrov <jeysonep@gmail.com>
2017-04-16 13:33:34 +0500

    Update after offline review

--- views/blocks/index/geolocation.js
+++ views/blocks/index/geolocation.js
@@ -1,18 +1,20 @@
 'use strict';
 
+const success = resolve => {
+    return position => {
+        resolve({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    };
+};
+
 export default () => {
     return new Promise((resolve, reject) => {
         if (!navigator.geolocation) {
             reject(new Error('This browser not support Web API "geolocation"'));
         }
 
-        const success = position => {
-            resolve({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-            });
-        };
-
-        navigator.geolocation.getCurrentPosition(success, reject);
+        navigator.geolocation.getCurrentPosition(success(resolve), reject);
     });
 };
