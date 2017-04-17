e8a482b3e72c6ab1a616a7d5b5a13ad5895fba6d
IrinaSed <irinasedova2011@gmail.com>
2017-04-17 19:06:03 +0500

    fixed lint in clear_db

--- scripts/clear-db.js
+++ scripts/clear-db.js
@@ -4,14 +4,13 @@ process.chdir(__dirname);
 const fs = require('fs');
 const mongoose = require('mongoose');
 
-
 const models = fs.readdirSync('../models')
-    .map((file) => require(`../models/${file}`));
+    .map(file => require(`../models/${file}`));
 
 require('../db/connect')()
     .then(() => {
         return Promise.all([
-            models.map((model) => {
+            models.map(model => {
                 return model.remove({}).exec();
             })
         ]);
