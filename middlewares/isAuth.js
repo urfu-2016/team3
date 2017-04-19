45a6779e0536308fa7e6e99164f585bc7055c6cf
Gallyam Biktashev <gbiktashev@naumen.ru>
2017-04-19 16:42:25 +0500

    Fix isAuth

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -4,6 +4,8 @@ module.exports = (req, res, next) => {
     if (req.isAuthenticated()) {
         return next();
     }
-    req.session.returnTo = req.url;
+    if (req.method === 'GET') {
+        req.session.returnTo = req.url;
+    }
     res.redirect('/login');
 };
--- routes/quest.js
+++ routes/quest.js
@@ -6,7 +6,7 @@ const isAuth = require('../middlewares/isAuth');
 module.exports = app => {
     app.route('/quests')
         .get(quests.list)
-        .post(quests.create);
+        .post(isAuth, quests.create);
     app.get('/quests/create', isAuth, quests.create);
     app.get('/quests/:id', quests.show);
     app.post('/quests/:id/publish', isAuth, quests.publish);
