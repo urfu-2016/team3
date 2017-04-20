055e9c8aeefbf333e090e9a5b7c49622f5258abd
Grehhov <grehhov@mail.ru>
2017-04-21 02:37:05 +0500

    Добавлена капча и решён вопрос с высотой блока

--- views/pages/createQuest/index.js
+++ views/pages/createQuest/index.js
@@ -6,4 +6,6 @@ import floatingMenu from '../../blocks/floating_menu';
 export default () => {
     header();
     floatingMenu();
+    const textarea = document.querySelector('.big-text__textarea');
+    textarea.classList.add('big-text__textarea_max-height');
 };
