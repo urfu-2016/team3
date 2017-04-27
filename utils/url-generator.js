efc9f20156a29dffcaba16afe4de8bc910f65691
Gallyam Biktashev <gallyamb@gmail.com>
2017-04-27 10:47:34 +0500

    Refactoring. Fix review issues

--- utils/url-generator.js
+++ utils/url-generator.js
@@ -1,35 +1,30 @@
 'use strict';
 
-const urljoin = require('url-join');
+const extractParameter = id => id || ':id';
 
-function extractParameter(id) {
-    return (id || ':id').toString();
-}
-
-let self;
-module.exports = self = { // eslint-disable-line no-multi-assign
+const self = module.exports = { // eslint-disable-line no-multi-assign
     common: {
         main: () => '/',
-        about: () => urljoin(self.common.main(), 'about')
+        about: () => `${self.common.main()}/about`
     },
     photos: {
         root: () => '/photos',
-        specific: id => urljoin(self.photos.root(), '{0}'.format([extractParameter(id)])),
-        image: id => urljoin(self.photos.root(), '{0}/image'.format([extractParameter(id)])),
-        checkin: id => urljoin(self.photos.root(), '{0}/checkin'.format([extractParameter(id)]))
+        specific: id => `${self.photos.root()}/${extractParameter(id)}`,
+        image: id => `${self.photos.specific(id)}/image`,
+        checkin: id => `${self.photos.specific(id)}/checkin`
     },
     quests: {
         root: () => '/quests',
-        specific: id => urljoin(self.quests.root(), '{0}'.format([extractParameter(id)])),
-        create: () => urljoin(self.quests.root(), 'create'),
-        publish: id => urljoin(self.quests.root(), '{0}/publish'.format([extractParameter(id)])),
-        comment: id => urljoin(self.quests.root(), '{0}/comment'.format([extractParameter(id)]))
+        create: () => `${self.quests.root()}/create`,
+        specific: id => `${self.quests.root()}/${extractParameter(id)}`,
+        publish: id => `${self.quests.specific(id)}/publish`,
+        comment: id => `${self.quests.specific(id)}/comment`
     },
     users: {
         profile: () => '/profile',
         login: () => '/login',
-        loginVK: () => urljoin(self.users.login(), 'vk'),
-        loginTwitter: () => urljoin(self.users.login(), 'twitter'),
+        loginVK: () => `${self.users.login()}/vk`,
+        loginTwitter: () => `${self.users.login()}/twitter`,
         register: () => '/register',
         logout: () => '/logout'
     }
