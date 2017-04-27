94405650158d7f0fc09315f4a1d0028fcb6e1054
Gallyam Biktashev <gallyamb@gmail.com>
2017-04-27 10:34:45 +0500

    Add forgotten files

---
+++ tests/integration/models/user-test.js
@@ -0,0 +1,23 @@
+const User = require('../../../models/user');
+const assert = require('assert');
+const mongoose = require('mongoose');
+
+describe('model: user', () => {
+    beforeEach(() => User.remove({}).exec());
+    before(() => require('../../../db/connect')());
+    after(() =>
+        User.remove({}).exec(() => mongoose.connection.close()));
+    it('save user', () => {
+        const user = new User({
+            likedQuests: [],
+            name: 'Anton',
+            email: 'Anton@gmail.com',
+            password: '3432423sdza',
+            passedQuests: [],
+            photoStatuses: []
+        });
+
+        return user.save()
+            .then(savedUser => assert.equal(savedUser.name, user.name));
+    });
+});
---
+++ tests/integration/models/user_follower-test.js
@@ -0,0 +1,20 @@
+const UserFollower = require('../../../models/user_follower');
+const User = require('../../../models/user');
+const assert = require('assert');
+const mongoose = require('mongoose');
+
+describe('model: userFollower', () => {
+    beforeEach(() => UserFollower.remove({}).exec());
+    before(() => require('../../../db/connect')());
+    after(() =>
+        UserFollower.remove({}).exec(() => mongoose.connection.close()));
+    it('save userFollower', () => {
+        const userFollower = new UserFollower({
+            userId: new User({}),
+            followedUserId: new User({})
+        });
+
+        return userFollower.save()
+            .then(savedUserFollower => assert.equal(savedUserFollower.userId, userFollower.userId));
+    });
+});
