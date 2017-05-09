018c256a3b866f8b1c9fb8bd567f1279788f57d1
Gallyam Biktashev <gallyamb@gmail.com>
2017-05-09 20:13:49 +0500

    Rename csrf

--- middlewares/common-data.js
+++ middlewares/common-data.js
@@ -7,7 +7,8 @@ module.exports = (req, res, next) => {
     if (req.user) {
         res.locals.user = req.user;
     }
-    res.locals.csrf = `<input type="hidden" name="_csrf" value="${req.csrfToken()}">`;
+    res.locals.csrfToken = req.csrfToken();
+    res.locals.csrfHiddenInput = `<input type="hidden" name="_csrf" value="${res.locals.csrfToken}">`;
     if (env.NODE_ENV === 'production') {
         res.locals.staticBasePath = '//team3.surge.sh';
     }
