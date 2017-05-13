07ff6944689730091a85a2bb480da49a0ba64119
Evgeny Petrov <jeysonep@gmail.com>
2017-05-13 10:09:24 +0500

    Update

--- views/pages/error/index.js
+++ views/pages/error/index.js
@@ -4,7 +4,7 @@ import header from '../../blocks/header';
 
 const errorRender = () => {
     const code = document.querySelector('.error-page__status-code').innerText.split('');
-    [...document.querySelectorAll('.photo-cards__content')].forEach((item, index) => {
+    [...document.querySelectorAll('.error-page__photo-cards__content')].forEach((item, index) => {
         item.innerText = code[index];
     });
 };
