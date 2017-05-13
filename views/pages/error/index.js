246de1bcb10e28aa374ede7ad8afef4205fcbb3c
Evgeny Petrov <jeysonep@gmail.com>
2017-05-13 07:26:29 +0500

    Fixing

--- views/pages/error/index.js
+++ views/pages/error/index.js
@@ -4,7 +4,9 @@ import header from '../../blocks/header';
 
 const errorRender = () => {
     const code = document.querySelector('.error-page__status-code').innerText.split('');
-    [...document.querySelectorAll('.photo-cards__content')].forEach((item, index) => item.innerText = code[index]);
+    [...document.querySelectorAll('.photo-cards__content')].forEach((item, index) => {
+        item.innerText = code[index];
+    });
 };
 
 export default () => {
