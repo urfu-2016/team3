431d049491fddeb95f1e7b23db981e3ff44bc1a7
Evgeny Petrov <jeysonep@gmail.com>
2017-05-17 16:25:24 +0500

    Удалил html-sanitizer.js

--- utils/html-sanitizer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const sanitizeHtml = require('sanitize-html');
-
-module.exports = html => {
-    return sanitizeHtml(html, {
-        allowedTags: ['b', 'i', 'u', 'div', 'li', 'ul', 'ol', 'span', 'br']
-    });
-};
