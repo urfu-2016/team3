5ef94d0b20ccc5b04d14942e2bf05b31767a45b2
Evgeny Petrov <jeysonep@gmail.com>
2017-05-17 09:27:40 +0500

    Fix

--- views/pages/createQuest/index.js
+++ views/pages/createQuest/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import header from '../../blocks/header';
-import floatingMenu from '../../blocks/floating_menu';
 import createQuest from '../../blocks/create_quest';
 
 export default () => {
