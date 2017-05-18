ef938fc003a70a78ff4b31e2bdea16352d62d0ae
Grehhov <grehhov@mail.ru>
2017-05-18 17:08:12 +0500

    add form for delete

--- views/blocks/quest_description/index.js
+++ views/blocks/quest_description/index.js
@@ -28,23 +28,12 @@ export default () => {
         });
     }
 
-    const removeQuestButton = document.querySelector('.quest-description__remove');
+    const removeQuestForm = document.querySelector('.quest-description__remove');
+    const removeQuestButton = removeQuestForm.querySelector('.floating-button');
     if (removeQuestButton) {
-        removeQuestButton.querySelector('.floating-button').classList.add('floating-button_red-background');
+        removeQuestButton.classList.add('floating-button_red-background');
         removeQuestButton.addEventListener('click', () => {
-            const questId = window.location.href.split('/').pop();
-            const url = window.location.origin + questUrls.remove(questId);
-            fetch(url, {
-                method: 'post',
-                headers: {
-                    'X-CSRF-Token': csrf.value
-                },
-                credentials: 'same-origin'})
-                .then(response => {
-                    if (response.redirected) {
-                        document.location.replace(response.url);
-                    }
-                });
+            removeQuestForm.submit();
         });
     }
 };
