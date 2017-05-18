6cd62e4fcd26c2f6d9c34d8efa5504f3086ab8d7
Gallyam Biktashev <gallyamb@gmail.com>
2017-05-18 17:27:28 +0500

    Closes #127. Use mongodb session store (#130)

---
+++ middlewares/recaptcha-error-handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = (req, res, next) => {
+    if (req.recaptcha.error) {
+        switch (req.recaptcha.error) {
+            case 'invalid-input-secret':
+            case 'missing-input-secret':
+                console.error(`reCaptcha secret key is ${req.recaptcha.error.split('-')[0]}`);
+                return next(new Error('There is a problem on our side. We already knows about it. Please, try again later.'));
+            case 'missing-input-response':
+                console.error('User\'s reCaptcha response was lost');
+                return next(new Error('There is a problem on our side. We already knows about it. Please, try again later.'));
+            case 'invalid-input-response':
+                return next(new Error('Your solution was wrong! Please, try again'));
+            default:
+                return next(new Error(`Undefined reCaptcha error: ${req.recaptcha.error}`));
+        }
+    }
+    next();
+};
